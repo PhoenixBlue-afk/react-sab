090d2237a0a0a67a80b4eace97a670325aff4e5e
PhoenixBlue-afk <82279633+PhoenixBlue-afk@users.noreply.github.com>
2021-08-28 23:28:25 -0300

    Borrado de app total por cambio de nombre

--- nombre-de-app/src/components/CartWidget/CartWidget.js
+++
@@ -1,9 +0,0 @@
-import { React } from "react";
-
-export const CartWidget = ()=> {
-    return <div className="arriba">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGWQbS5Rlk6NxxeYs0PR_Ax9VgN_QQsZoQUXExJjBKoXMVZ3H1HgkjCl1kL_I&usqp=CAc" alt="foto" className="logo" />
-        
-        
-    </div>
-}
