e1750837d295c3a893bba96333bea9011d239248
PhoenixBlue-afk <82279633+PhoenixBlue-afk@users.noreply.github.com>
2021-09-18 11:51:12 -0300

    Add files via upload
    carrito se muestra condiconal

--- app/src/components/ShoppingCart/ShoppingCart.js
+++ app/src/components/ShoppingCart/ShoppingCart.js
@@ -6,14 +6,21 @@ import {CarritoContext} from '../../context/CarritoContext'
 
 export const ShoppingCart =() =>{
 
-const {acumuladorCarrito} = useContext(CarritoContext)
-    
-    return(
-           <Link to="/Carrito">
+const {acumuladorCarrito, carrito} = useContext(CarritoContext)
+    return(<div>
+        {(carrito == 0) ? 
+            <p>sin carrito</p>
+            :
+            <Link to="/Carrito">
                 <div>
                     <FaShoppingCart id="icono" />
                     <span>{acumuladorCarrito()}</span>
                 </div>
             </Link> 
+        
+        }
+    </div>
+        
+           
     )
 }
